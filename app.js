01c36ec626a4d52b44c31ea1b869159ed1dd594b
sameerkhan00 <sk2655391@gmail.com>
2020-07-29 00:21:10 +0500

    counter

---
+++ app.js
@@ -0,0 +1,13 @@
+function add(){
+    var count=document.getElementById("number").innerHTML;
+  count++;
+  var count=document.getElementById("number").innerHTML=count;
+  
+  }
+  function lower(){
+      var count=document.getElementById("number").innerHTML;
+  count--;
+  var count=document.getElementById("number").innerHTML=count;
+  
+  
+  }
